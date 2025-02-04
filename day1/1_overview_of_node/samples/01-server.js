35043c18f54c1309382689fb50eaf551a84366fe
Kevin Cunningham <github@kevincunningham.co.uk>
2025-02-04 11:17:01 +0000

    adds dependencies

--- day1/1_overview_of_node/samples/01-server.js
+++ day1/1_overview_of_node/samples/01-server.js
@@ -1,9 +1,9 @@
-const http = require('http');
-
+const http = require("http");
+import http from "";
 const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.write('<h1>Welcome to Node.js!</h1>');
-	res.end();
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Welcome to Node.js!</h1>");
+  res.end();
 });
 
 server.listen(3000);
